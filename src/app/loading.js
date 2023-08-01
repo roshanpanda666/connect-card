8687d6cbd32ba35ea641cb74018975f05b13ed70
Roshan panda <roshanpanda666@gmail.com>
2023-08-01 20:40:52 +0530

    commiting

---
+++ src/app/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <div className='text-white'>
+        loading.....
+      </div>
+    </div>
+  )
+}
+
+export default Loading
